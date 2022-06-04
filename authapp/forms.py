e8f1d75065bc2bdc40e4ee66863b1f2fd054891c
AlexMomot-717 <momot-717@yandex.ru>
2022-06-04 16:03:04 +0300

    change article method delete, delete unused code

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,16 +1,6 @@
-import hashlib
-from random import random
-
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from authapp.models import HabrUser, HabrUserProfile
 from django import forms
-
-from datetime import datetime, timedelta
-
-import pytz
-from django.conf import settings
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 
 class HabrUserLoginForm(AuthenticationForm):
@@ -40,14 +30,8 @@ class HabrUserRegisterForm(UserCreationForm):
         user = super().save(*args, **kwargs)
         user.is_active = False
 
-        # salt = hashlib.sha1(str(random.random()).encode('utf8')).hexdigest()[:6]
-        # user.activate_key = hashlib.sha1((user.email + salt).encode('utf8')).hexdigest()
-
-        user.activate_key = hashlib.sha1(user.email.encode('utf8')).hexdigest()
-        user.activate_key_expired = datetime.now(pytz.timezone(settings.TIME_ZONE))
-
         user.save()
-        return  user
+        return user
 
 
 class HabrUserEditForm(UserChangeForm):
