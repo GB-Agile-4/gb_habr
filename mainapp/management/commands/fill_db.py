6ad7525f5a0c9aa581451e874e4f933c3412adf0
Igor <igor.kerro@mail.ru>
2022-06-10 23:29:45 +0300

    refactor: fill_db.py

--- mainapp/management/commands/fill_db.py
+++ mainapp/management/commands/fill_db.py
@@ -1,10 +1,11 @@
-from django.core.management.base import BaseCommand
-from mainapp.models import ArticleCategory
-from article.models import Article
+import os
+import json
 
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-import json, os
+from mainapp.models import ArticleCategory
+from article.models import Article
 
 
 JSON_PATH = 'mainapp/json'
@@ -19,11 +20,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         categories = load_from_json('categories')
         ArticleCategory.objects.all().delete()
+
         for category in categories:
             new_category = ArticleCategory(**category)
             new_category.save()
             articles = load_from_json('articles')
             Article.objects.all().delete()
+
         for article in articles:
             category_name = article["category"]
             # Получаем категорию по имени
@@ -32,6 +35,6 @@ class Command(BaseCommand):
             article['category'] = _category
             new_article = Article(**article)
             new_article.save()
+
     # Создаем суперпользователя при помощи менеджера модели
-    super_user = User.objects.create_superuser('django',
-    'django@gbhabr.local', 'gbhabr')
+    super_user = User.objects.create_superuser('django', 'django@gbhabr.local', 'gbhabr')
