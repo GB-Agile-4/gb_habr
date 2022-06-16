20a67ac6937b1204ceb2dfa9a1439c76d7d95b98
AlexMomot-717 <momot-717@yandex.ru>
2022-06-16 20:01:08 +0300

    remove companies, news & authors

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,8 +6,5 @@ app_name = 'content'
 
 urlpatterns = [
     path('articles/<int:pk>/', mainapp.articles, name='articles'),
-    path('news/', mainapp.news, name='news', ),
     path('hubs/', mainapp.hubs, name='hubs', ),
-    path('authors/', mainapp.authors, name='authors', ),
-    path('companies/', mainapp.companies, name='companies', ),
 ]
