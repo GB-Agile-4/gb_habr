e382cfaa0657a1669ae485672c2cff84defd1153
Igor <igor.kerro@mail.ru>
2022-06-19 00:31:28 +0300

    refacor: urls

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from mainapp import views as mainapp
 
 
-app_name = 'content'
+app_name = 'mainapp'
 
 urlpatterns = [
+    path('', mainapp.index, name='index'),
     path('articles/<int:pk>/', mainapp.articles, name='articles'),
     path('hubs/', mainapp.hubs, name='hubs', ),
 ]
