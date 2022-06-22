59495380e052c8ff671582af9530dd97f4649126
AlexMomot-717 <momot-717@yandex.ru>
2022-06-22 21:16:51 +0300

    register comment model in admin.py

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from articleapp.models import Article, ArticleCategory
 from authapp.models import HabrUser, HabrUserProfile
+from commentapp.models import Comment
+
 
 admin.site.register(ArticleCategory)
-admin.site.register(Article)
 admin.site.register(HabrUser)
 admin.site.register(HabrUserProfile)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'author', 'body', 'created_at', 'updated_at', 'is_active', 'is_moderated', 'is_archived')
+    list_filter = ('category', 'author', 'is_active', 'created_at', 'updated_at', 'is_active', 'is_moderated')
+    search_fields = ('title', 'body')
+
+
+admin.site.register(Article)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment_author', 'article', 'created_at', 'updated_at', 'is_active', 'is_moderated')
+    list_filter = ('is_active', 'created_at', 'updated_at')
+    search_fields = ('comment_author', 'body')
+
+
+admin.site.register(Comment, CommentAdmin)
