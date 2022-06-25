cd29e1bd09b889e09e492c163f1ddfe9c5b9ea78
AlexMomot-717 <momot-717@yandex.ru>
2022-06-25 09:04:13 +0300

    update adminapp/admin.py

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -4,18 +4,10 @@ from authapp.models import HabrUser, HabrUserProfile
 from commentapp.models import Comment
 
 
-admin.site.register(ArticleCategory)
-admin.site.register(HabrUser)
-admin.site.register(HabrUserProfile)
-
-
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('category', 'title', 'author', 'body', 'created_at', 'updated_at', 'is_active', 'is_moderated', 'is_archived')
     list_filter = ('category', 'author', 'is_active', 'created_at', 'updated_at', 'is_active', 'is_moderated')
     search_fields = ('title', 'body')
-
-
-admin.site.register(Article)
 
 
 class CommentAdmin(admin.ModelAdmin):
@@ -24,4 +16,8 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ('comment_author', 'body')
 
 
+admin.site.register(ArticleCategory)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(HabrUser)
+admin.site.register(HabrUserProfile)
 admin.site.register(Comment, CommentAdmin)
