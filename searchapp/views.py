a79381d9abfad2356af783d5a1daeddd504b1d70
IgoryanM <igor.kerro@mail.ru>
2022-06-27 00:49:49 +0300

    feat(searchapp): add sorting and image.

--- searchapp/views.py
+++ searchapp/views.py
@@ -4,12 +4,20 @@ from articleapp.models import Article
 
 
 def search(request):
-    if request.method == 'POST':
-        title = request.POST.get('to_search')
+    sort_order = {'title': 'По названию',
+                  'created_at': 'Сначала свежие',
+                  'rating': 'Сначала с высоким рейтингом'}
 
-        if title:
-            articles = Article.objects.filter(title__contains=title)
-            context = {'articles': articles}
-            return render(request, 'searchapp/search.html', context=context)
+    if request.method == 'POST':
+        title = request.POST.get('title')
+
+    else:
+        title = request.GET.get('title')
+
+    if title:
+        order = request.GET.get('order', 'title')
+        articles = Article.objects.filter(title__contains=title).order_by(f'-{order}')
+        context = {'articles': articles, 'query': title, 'order': sort_order.get(order)}
+        return render(request, 'searchapp/search.html', context=context)
 
     return render(request, 'searchapp/search.html')
