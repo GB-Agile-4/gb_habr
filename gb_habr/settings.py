4a3c8d5d87912c73db0e4d6b565017184cc96609
AlexMomot-717 <momot-717@yandex.ru>
2022-06-30 22:08:56 +0300

    return to sqlite

--- gb_habr/settings.py
+++ gb_habr/settings.py
@@ -84,14 +84,21 @@ WSGI_APPLICATION = 'gb_habr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gbhabr',
-        'USER': 'gbhabr',
-        'PASSWORD': 'gbhabr',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'gbhabr',
+#         'USER': 'gbhabr',
+#         'PASSWORD': 'gbhabr',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
