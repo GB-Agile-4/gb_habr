cd82301f09512d93b43f294bb01d490c4b041d46
AlexMomot-717 <momot-717@yandex.ru>
2022-07-03 22:18:31 +0300

    create CommentMark model

--- likeapp/models.py
+++ likeapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from authapp.models import HabrUser
 from articleapp.models import Article
+from commentapp.models import Comment
 
 
 class Mark(models.Model):
@@ -11,4 +12,15 @@ class Mark(models.Model):
 
     habruser = models.ForeignKey(HabrUser, on_delete=models.CASCADE, related_name='marks')
     marked_article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='marks')
+    mark = models.CharField(max_length=7, choices=CHOICES, default=None)
+
+
+class CommentMark(models.Model):
+    CHOICES = (
+        ("LIKE", "like"),
+        ("DISLIKE", "dislike")
+    )
+
+    habruser = models.ForeignKey(HabrUser, on_delete=models.CASCADE, related_name='comment_marks')
+    marked_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='comment_marks')
     mark = models.CharField(max_length=7, choices=CHOICES, default=None)
