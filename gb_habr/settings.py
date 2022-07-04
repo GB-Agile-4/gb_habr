e5ffc6dfe7d820b293379cb20f720617200a0b2b
AlexMomot-717 <momot-717@yandex.ru>
2022-07-04 13:59:40 +0300

    change db

--- gb_habr/settings.py
+++ gb_habr/settings.py
@@ -86,14 +86,21 @@ WSGI_APPLICATION = 'gb_habr.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'gbhabr',
+#         'USER': 'gbhabr',
+#         'PASSWORD': 'gbhabr',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gbhabr',
-        'USER': 'gbhabr',
-        'PASSWORD': 'gbhabr',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
