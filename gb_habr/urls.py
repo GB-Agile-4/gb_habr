8810db3a1dbbc228adbf38d664ce7347f5da1fd5
AlexMomot-717 <76219029+AlexMomot-717@users.noreply.github.com>
2022-07-05 10:32:06 +0300

    Update urls.py

--- gb_habr/urls.py
+++ gb_habr/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 
 from mainapp import views as mainapp
+from articleapp import views as articleapp
 
 
 urlpatterns = [
