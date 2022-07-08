c8898ce75bc39e6cff76ef489b8b8f6802e1aa1b
nnt <enenty@yandex.ru>
2022-07-08 15:43:48 +0300

    Added background unban scheduler. Added 2-week bans

--- moderapp/views.py
+++ moderapp/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler as BS
+
 from articleapp.models import Article
 from authapp.models import HabrUser
+
+
+def start_scheduler():
+    scheduler = BS()
+    scheduler.add_job(check_scheduled_unbans, "interval", minutes=10, id="unban_manager", replace_existing=True)
+    scheduler.start()
+
+
+def check_scheduled_unbans():
+    print("Checking expired bans...")
+    today = datetime.now().date()
+    banned_users = HabrUser.objects.filter(is_banned=True)
+    for user in banned_users:
+        if user.banned_till <= today:
+            user.is_banned = False
+            user.save()
 
 
 def ban_user(request, pk):
@@ -13,7 +32,11 @@ def ban_user(request, pk):
     if habr_user.is_banned:
         habr_user.is_banned = False
     else:
+        time_now = datetime.now().date()
         habr_user.is_banned = True
+        habr_user.banned_till = time_now + timedelta(days=14)
+        print(habr_user.banned_till)
+
     habr_user.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
